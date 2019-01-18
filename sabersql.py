b391ec9e6cf1c35735ecaf43c532c0bebd91dc68
Liam Stevenson <liam923@verizon.net>
2019-01-18 18:32:13 -0500

    Initialize project

---
+++ sabersql.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+from sabersql.__main__ import run
+run()
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sabersql",
+    version="0.0.1",
+    author="William Stevenson",
+    author_email="liam923@verizon.net",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liam923/sabersql",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'sabersql = sabersql.__main__:run',
+        ],
+    }
+)
