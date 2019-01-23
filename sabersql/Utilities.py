399791f54e64633cdb2e935896613f98026da10a
Liam Stevenson <liam923@verizon.net>
2019-01-22 21:02:46 -0500

    Created _shell and test

--- sabersql/Utilities.py
+++ sabersql/Utilities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import subprocess
 
 def _download(url, target):
     """
@@ -8,4 +9,22 @@ def _download(url, target):
     :param target: the location to download the file to
     :return: None
     """
+
+
     pass
+
+def _shell(command):
+    """
+    Calls a shell command
+
+    :param command: the command to be run
+    :return: (stdOut, stdErr)
+    """
+
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    if out is not None:
+        out = out.decode('utf-8')
+    if err is not None:
+        err = err.decode('utf-8')
+    return out, err
---
+++ tests/test_Utilities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from unittest import TestCase
+import sabersql.Utilities
+
+class TestUtilities(TestCase):
+
+    def test_shell(self):
+        out, err = sabersql.Utilities._shell("echo hi")
+        self.assertEqual("hi\n", out)
+        self.assertEqual("", err)
+
+        out, err = sabersql.Utilities._shell(">&2 echo hi")
+        self.assertEqual("", out)
+        self.assertEqual("hi\n", err)
